4a0fc3f8e94f66cb900e7f077ab006b75cb6fdb0
sabiha <sabihamumcu@outlook.com>
2025-11-23 23:02:20 +0300

    kotlin version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     id("com.google.gms.google-services")
     alias(libs.plugins.firebase.crashlytics)
@@ -39,9 +40,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
     }
     packaging {
         resources {
